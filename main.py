4901ec06dbf7b7e3beb7ebf2ecc44545f2482d4c
Sankrub <navathon.l@ku.th>
2023-02-08 13:40:11 +0700

    Add comment

--- main.py
+++ main.py
@@ -16,35 +16,45 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+
 class light(BaseModel):
-    light_id:int
+    light_id: int
     status: bool
     mode: str
     brightness: int
+
+
 # get all data
 @app.get("/")
 def get_all_light():
     return list(mongo_connection["light"].find({}, {"_id": 0}))
 
+
+# get light information
 @app.get("/{id}")
 def get_light(id: int):
     if id < 1 or id > 3:
         raise HTTPException(404, detail="light id is invalid")
     return dict(mongo_connection["light"].find_one({"light_id": id}, {"_id": 0}))
+
+
 # mock data
-data=[
-    {"light_id":1,"status":True,"mode":"AUTO","brightness":100},
-    {"light_id":2,"status":False,"mode":"DISCO","brightness":0},
-    {"light_id":3,"status":True,"mode":"MANUAL","brightness":100},
+data = [
+    {"light_id": 1, "status": True, "mode": "AUTO", "brightness": 100},
+    {"light_id": 2, "status": False, "mode": "DISCO", "brightness": 0},
+    {"light_id": 3, "status": True, "mode": "MANUAL", "brightness": 100},
 ]
+
+
 # add mock data
 @app.post("/AddMock")
 def post_light():
     mongo_connection["light"].insert_many(data)
 
+
 @app.put("/update")
 def update_light(light_object: light):
-    enum_mode = ["AUTO","MANUAL","DISCO"]
+    enum_mode = ["AUTO", "MANUAL", "DISCO"]
     if light_object.light_id < 1 or light_object.light_id > 3:
         raise HTTPException(status_code=400, detail="LightID out of range")
     if light_object.mode not in enum_mode:
@@ -52,10 +62,13 @@ def update_light(light_object: light):
     if light_object.brightness < 0 or light_object.brightness > 100:
         raise HTTPException(status_code=400, detail="Brightness not in range")
     if light_object.mode == "AUTO" or light_object.mode == "DISCO":
-        mongo_connection["light"].update_one({"light_id": light_object.light_id},{"$set": {"status": light_object.status, "mode": light_object.mode}})
+        mongo_connection["light"].update_one({"light_id": light_object.light_id},
+                                             {"$set": {"status": light_object.status, "mode": light_object.mode}})
         return {"success": True, "detail": "Brightness unchanged"}
-    mongo_connection["light"].update_one({"light_id": light_object.light_id},{"$set": {"status": light_object.status, "mode": light_object.mode, "brightness": light_object.brightness}})
+    mongo_connection["light"].update_one({"light_id": light_object.light_id}, {
+        "$set": {"status": light_object.status, "mode": light_object.mode, "brightness": light_object.brightness}})
     return {"success": True}
+
 
 # delete all data
 @app.delete("/")
